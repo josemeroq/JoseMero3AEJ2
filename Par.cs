54db2f3b6da19a9a0fa39551763d044f57d70d91
josemeroq <JAMQ1316762374@gmail.com>
2022-05-15 21:18:57 -0500

    Agregar archivos de proyecto.

---
+++ Par.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JoseMero3AEJ2
+{
+    public class Par
+    {
+        private int num { set; get; }
+
+        public Par(int num)
+        {
+            this.num = num;
+        }
+
+        public void proceso()
+        {
+            if(num % 2 == 0)
+            {
+                Console.WriteLine("El número " + num + " es par ");
+            }
+            else
+            {
+                Console.WriteLine("El número " + num + " no es par ");
+            }
+            Console.ReadKey();
+        }
+    }
+    
+
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JoseMero3AEJ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Par numpar = new Par(6);
+            numpar.proceso();
+        }
+    }
+}
